828f170dd8a566f399230fb510ab877e4c2a8d1c
ArneJohnson <arnejohnson02@gmail.com>
2024-09-01 21:59:58 +0200

    Update lab1-2-v2.js

--- experiments/lab1-2-v2.js
+++ experiments/lab1-2-v2.js
@@ -1,39 +1,44 @@
 const size = 50;
-const amount = 6;
+const amount = 5;
 const gap = 5;
 var t = 0;
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
   rectMode(CENTER);
-  frameRate(30);
+  frameRate(60);
+  background(255);
 }
 
 function draw() {
   let noiseScale = 0.01;
-  let noiseLevel = 100;
+  let noiseLevel = 50;
   let nt = noiseScale * frameCount;
   let count = 0;
 
-  background(255);
-  const centerX = width / 2;
-  const centerY = height / 2;
-  for (let x = -Math.floor(amount / 2); x < Math.ceil(amount / 2); x++) {
-    for (let y = -Math.floor(amount / 2); y < Math.ceil(amount / 2); y++) {
-      count++;
-      var percentage = count / (amount * amount);
+  noStroke();
+  fill(255, 10);
+  rect(0, 0, innerWidth * 2, innerHeight * 2);
 
-      let xPosition = centerX + x * (size + gap) + percentage * noiseLevel * noise(nt);
-      let yPosition = centerY + y * (size + gap) + percentage * noiseLevel * noise(nt);
+  const centerX = width / 2;
+  const centerY = height / 4;
+  for (let y = 0; y < amount; y++) {
+    for (let x = -Math.floor(amount / 2); x < Math.ceil(amount / 2); x++) {
+      count++;
+      var percentage = y * y / amount;
+
+      let xPosition = centerX + x * (size + gap);
+      let yPosition = centerY + y * (size + gap) + (percentage * noiseLevel * noise(nt));
       if (amount % 2 === 0) {
         xPosition += size / 2;
       }
       push();
       translate(xPosition, yPosition);
+      rotate(percentage * noise(nt));
       noFill();
       stroke(0);
       strokeWeight(2);
-      square(0, 0, size * 1.3 * percentage * noise(nt));
+      square(0, 0, size);
       pop();
     }
   }
